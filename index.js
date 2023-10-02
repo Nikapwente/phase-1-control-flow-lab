a1633f28b98ced37c665dae092315a89e2cbf63a
Nikapwente <hrsmajor@gmail.com>
2023-10-02 23:36:05 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  if (rideLength <= 400) {
+    return "This one is on me!"
+    }
+
+    else if (rideLength >= 2500) {
+      return "No can do."
+    }
+
+    else return "I will gladly take your thirty bucks."
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let toGo;
+  (cityName == "NYC")? (toGo = "Ok, sounds good.") : (toGo = "No go.")
+  return toGo;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipSize){
+  switch (tipSize) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+        break;
+    default:
+      return "Bye.";
+
+  }
+
 }
